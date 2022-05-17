a688409fd235d886cb53d1fa40b6feef2c3c1523
Mathius <mathiuskormasela12@gmail.com>
2022-05-17 20:32:22 +0700

    boilerplate project initialization

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
+// =========== App
+// import all module
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App: React.FC = () => (
+  <div>
+    <h1>Vehicle Rental Web</h1>
+    <Title>Test</Title>
+  </div>
+);
 
 export default App;
+
+const Title = styled.h1`
+	color: red;
+`;
